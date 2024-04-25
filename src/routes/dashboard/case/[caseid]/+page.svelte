515a96ae084e004a3ff00394c820238246910098
Serdar Korur <serdar.korur@gmail.com>
2024-04-25 10:56:51 +0200

    Show important details on Results page. Design fixes.

--- src/routes/dashboard/case/[caseid]/+page.svelte
+++ src/routes/dashboard/case/[caseid]/+page.svelte
@@ -1,76 +1,91 @@
 <script>
+  import { formatDistanceToNowStrict } from 'date-fns'
+  import { History, Ban } from 'lucide-svelte'
   export let data
-  // $: console.log({data})
-  let stepsExpanded
 </script>
 
-<div class="container mx-auto p-4">
-  <!-- Procedure Name -->
-  <h1 class="text-2xl font-bold mb-4">{data.case.procedure_name}</h1>
-  <h2 class="">Status</h2>
-  <h3 class="mb-4 uppercase font-bold">{data.case.status}</h3>
+<div class="container mx-auto p-4 max-w-2xl">
+  Procedure Name
+  <h1 class="text-3xl font-bold mb-4">{data.case.procedure_name}</h1>
+  <div class="flex flex-col sm:flex-row justify-between">
+    <!-- Final Determination -->
+    <div>
+      <h2 class="">Status</h2>
+      <h3 class="mb-4 uppercase font-bold">{data.case.status}</h3>
+    </div>
+    <div class="mb-4">
+      <h2 class="">Final Determination:</h2>
+      <p class="mb-4 uppercase font-bold">
+        {#if data.case.is_met}
+          <span class="text-green-600"> Met</span>
+        {:else}
+          <span class="flex items-center gap-2"><Ban size="16" /> Not Met </span>
+        {/if}
+      </p>
+    </div>
 
-  <!-- Final Determination -->
-  <div class="mb-4">
-    <h2 class="">Final Determination:</h2>
-    <p class="mb-4 uppercase font-bold">
-      {#if data.case.is_met}
-        Met
-      {:else}
-        Not Met
-      {/if}
-    </p>
-  </div>
-  <!-- CPT Codes -->
-  <div class="mb-4">
-    <h2 class="text-xl font-semibold mb-2">CPT Codes:</h2>
-    <ul class=" pl-4 flex gap-4 flex-col sm:flex-row">
-      {#each data.case.cpt_codes as code}
-        <div class="bg-gray-800 text-white rounded-xl px-3 py-2 max-w-min">
-          <li>{code}</li>
-        </div>
-      {/each}
-    </ul>
+    <!-- Relative Time -->
+    <div class="mb-4">
+      <h2 class="">Created at:</h2>
+
+      <p class="mb-4 font-bold flex items-center gap-1">
+        <History size="16" />
+        {formatDistanceToNowStrict(new Date(data.case.created_at))} ago
+      </p>
+    </div>
+
+    <div class="mb-4">
+      <h2 class="">Record id:</h2>
+
+      <p class="mb-4 font-bold">
+        {data.case.id}
+      </p>
+    </div>
   </div>
 
   <!-- Case Summary -->
   <div class="mb-4">
-    <h2 class="text-xl font-semibold mb-2">Case Summary:</h2>
-    <p>{data.case.summary}</p>
-  </div>
-
-  <!-- Relative Time -->
-  <div class="mb-4">
-    <h2 class="text-xl font-semibold mb-2">Relative Time Since Created:</h2>
-    <p>{new Date(data.case.created_at).toLocaleString()}</p>
+    <h2 class="text-xl font-semibold mb-4">
+      Case Summary:
+      <span class="{data.case.is_met ? 'text-green-600' : 'text-red-600'} uppercase">
+        {data.is_met ? 'Approved' : 'Denial'}
+      </span>
+    </h2>
+    <p
+      class="{data.case.is_met
+        ? 'bg-green-200 border-2 border-red-600'
+        : 'bg-red-200 border-2 border-red-600'} p-3 rounded-xl"
+    >
+      {data.case.summary}
+    </p>
   </div>
 
   <!-- Steps Taken -->
-  <!-- <div class="mb-4">
-    <h2 class="text-xl font-semibold mb-2">Steps Taken:</h2>
-    <ul class="list-disc pl-4">
+  <div class="mb-4 mt-8">
+    <h2 class="text-xl font-semibold mb-2">Steps</h2>
+    <ul>
       {#each data.case.steps as step}
-        <li>{step.reasoning}</li>
+        <div class="my-4 border-2 border-gray-400 rounded-xl p-3 bg-gray-50">
+          <h1 class="text-xl mb-4 font-bold">{step.question}</h1>
+          <div class="flex flex-col gap-2">
+            {#each step.reasoning.split('\n') as lines}
+              <li>{lines}</li>
+            {/each}
+          </div>
+        </div>
       {/each}
     </ul>
-  </div> -->
-
-  <div class="mb-4">
-    <button
-      class="flex justify-between cursor-pointer"
-      on:click={() => (stepsExpanded = !stepsExpanded)}
-    >
-      <h2 class="text-xl font-semibold mb-2">Steps Taken:</h2>
-      <span class="transform" class:rotate-180={stepsExpanded} style="transition: transform 0.3s;"
-        >&#9654;</span
-      >
-    </button>
-    {#if stepsExpanded}
-      <div class="bg-gray-100 p-4 mt-2">
-        <p>{data.case.steps[0].reasoning}</p>
-        <!-- Displaying only the first sentence -->
-      </div>
-    {/if}
+  </div>
+  <!-- CPT Codes -->
+  <div class="mb-8 mt-8">
+    <h2 class="text-xl font-semibold mb-2">CPT Codes:</h2>
+    <ul class=" pl-4 flex flex-wrap gap-4">
+      {#each data.case.cpt_codes as code}
+        <div class="bg-gray-700 text-white rounded-xl px-3 py-2 max-w-min">
+          <li>{code}</li>
+        </div>
+      {/each}
+    </ul>
   </div>
 </div>
 <!-- <div class="font-bold text-2xl">CASE OVERVIEW</div>
